41a5c3bfa1b4eef0bbb7725e998a8a5360930b51
Ahmed Hesham <ah0115915v@gmail.com>
2026-08-26 15:34:21 +0300

    Task 1

---
+++ Assignment2_C#/classes/Product .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System;
+using System.Collections.Generic;
+
+namespace Assignment2_C_.classes
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
---
+++ Assignment2_C#/classes/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2_C_.classes
+{
+    internal class ProductService
+    {
+
+        public List<Product> SearchProducts(List<Product> products, Func<Product, bool> filter)
+        {
+            List<Product> result = new List<Product>();
+            foreach (Product product in products)
+            {
+                if (filter(product))
+                {
+                    result.Add(product);
+                }
+            }
+            return result;
+
+        }
+
+    }
+}
