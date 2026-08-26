798364e7f080a29eeca8099050afc960cae6c181
Ahmed Hesham <ah0115915v@gmail.com>
2026-08-26 14:37:47 +0300

    Add project files.

---
+++ Assignment2_C#/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+namespace Assignment2_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
